67cc504836ebfef0eb4b0266ca6c5666cc32de74
L3NNX <gauravboy1122@gmail.com>
2024-07-12 10:45:08 +0530

    Upload image using multer & create Product

--- routes/productsRouters.js
+++ routes/productsRouters.js
@@ -1,9 +1,28 @@
 const express=require('express');
-
 const router= express.Router();
+const upload= require("../config/multerConfig")
+const productModel = require("../models/productmodel")
 
-router.use('/',(req,res)=>{
-    res.send("Welcome2");
+router.post("/create",upload.single("image"),async function (req,res){
+    try {
+        let {name,price,discount,bgcolor,textcolor,panelcolor} = req.body;
+        
+        let product = await productModel.create({
+            image: req.file.buffer,
+            name,
+            price,
+            discount,
+            bgcolor,
+            textcolor,
+            panelcolor,
+        })
+        req.flash("success","product created successfully")
+        res.redirect("/owners/admin")
+        
+    } catch (error) {
+        res.send(error)
+    }
+
 }); 
 
 module.exports = router;
