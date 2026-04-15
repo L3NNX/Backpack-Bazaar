c1142f5f134dccb9cf2a7440b5a0c861005d56d4
L3NNX <gauravboy1122@gmail.com>
2026-04-15 12:00:00 +0530

    feat: redesign homepage,header,footer,auth

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./views/**/*.ejs"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      colors: {
+        noir: { DEFAULT: '#080808', card: '#0d0d0d', input: '#111111', border: '#1c1c1c', hover: '#161616' },
+        gold: { DEFAULT: '#c9a96e', light: '#d4ba85', dark: '#a88a52' },
+        dim:  { 100: '#ffffff', 200: '#cccccc', 300: '#999999', 400: '#555555', 500: '#333333' }
+      },
+      fontFamily: {
+        display: ['"Cormorant Garamond"', 'Georgia', 'serif'],
+        sans: ['"DM Sans"', 'Helvetica Neue', 'sans-serif'],
+      }
+    }
+  }
 }
